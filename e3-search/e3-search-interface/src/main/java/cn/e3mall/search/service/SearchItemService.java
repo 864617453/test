aad2b2e2d9c5b9dc28ae6b19b2490c981a0afe88
864617453 <chcetc@gmail.com>
2018-06-20 06:41:41 -0600

    solr业务模块

---
+++ e3-search/e3-search-interface/src/main/java/cn/e3mall/search/service/SearchItemService.java
@@ -0,0 +1,8 @@
+package cn.e3mall.search.service;
+
+import Utils.E3Result;
+
+public interface SearchItemService {
+
+	E3Result importAllItems();
+}
---
+++ e3-search/e3-search-service/src/main/java/cn/e3mall/search/mapper/ItemMapper.java
@@ -0,0 +1,9 @@
+package cn.e3mall.search.mapper;
+
+import java.util.List;
+
+import pojo.SearchItem;
+
+public interface ItemMapper {
+List<SearchItem> getItemList();
+}
